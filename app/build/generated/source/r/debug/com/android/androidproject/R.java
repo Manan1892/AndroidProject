bac37f1305f62960f27f3d90dbba9d7463eb2a86
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-08 14:50:03 -0700

    project

--- app/build/generated/source/r/debug/com/android/androidproject/R.java
+++ app/build/generated/source/r/debug/com/android/androidproject/R.java
@@ -11,7 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int MyColor=0x7f050000;
+        public static final int MyColor=0x7f070000;
+        public static final int colorAccent=0x7f070001;
+        public static final int colorPrimary=0x7f070002;
+        public static final int colorPrimaryDark=0x7f070003;
+    }
+    public static final class dimen {
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int aboutus=0x7f020000;
@@ -26,78 +33,81 @@ public final class R {
         public static final int vinimo=0x7f020009;
     }
     public static final class id {
-        public static final int bHome=0x7f090004;
-        public static final int bNextPage=0x7f090005;
-        public static final int bSkip=0x7f090017;
-        public static final int bStartNow=0x7f090003;
-        public static final int bStartTest=0x7f09000b;
-        public static final int bnext=0x7f090016;
-        public static final int buttonCreateAccount=0x7f09001c;
-        public static final int buttonSignIN=0x7f09000e;
-        public static final int buttonSignUP=0x7f09000f;
-        public static final int editTextConfirmPassword=0x7f09001b;
-        public static final int editTextPassword=0x7f09001a;
-        public static final int editTextPasswordToLogin=0x7f09000d;
-        public static final int editTextUserName=0x7f090019;
-        public static final int editTextUserNameToLogin=0x7f09000c;
-        public static final int imageView1=0x7f090007;
-        public static final int mAboutUs=0x7f09001e;
-        public static final int mChronomete=0x7f090010;
-        public static final int mExit=0x7f09001f;
-        public static final int op1=0x7f090012;
-        public static final int op2=0x7f090013;
-        public static final int op3=0x7f090014;
-        public static final int op4=0x7f090015;
-        public static final int radioGroup1=0x7f090000;
-        public static final int rbEasy=0x7f090008;
-        public static final int rbHard=0x7f09000a;
-        public static final int rbLearnTech=0x7f090001;
-        public static final int rbMedium=0x7f090009;
-        public static final int rbSkillTesting=0x7f090002;
-        public static final int s=0x7f090018;
-        public static final int textView1=0x7f09001d;
-        public static final int textView2=0x7f090006;
-        public static final int tvQues1=0x7f090011;
+        public static final int bHome=0x7f0b0004;
+        public static final int bNextPage=0x7f0b0005;
+        public static final int bSkip=0x7f0b0017;
+        public static final int bStartNow=0x7f0b0003;
+        public static final int bStartTest=0x7f0b000b;
+        public static final int bnext=0x7f0b0016;
+        public static final int buttonCreateAccount=0x7f0b001c;
+        public static final int buttonSignIN=0x7f0b000e;
+        public static final int buttonSignUP=0x7f0b000f;
+        public static final int editTextConfirmPassword=0x7f0b001b;
+        public static final int editTextPassword=0x7f0b001a;
+        public static final int editTextPasswordToLogin=0x7f0b000d;
+        public static final int editTextUserName=0x7f0b0019;
+        public static final int editTextUserNameToLogin=0x7f0b000c;
+        public static final int imageView1=0x7f0b0007;
+        public static final int mAboutUs=0x7f0b001e;
+        public static final int mChronomete=0x7f0b0010;
+        public static final int mExit=0x7f0b001f;
+        public static final int op1=0x7f0b0012;
+        public static final int op2=0x7f0b0013;
+        public static final int op3=0x7f0b0014;
+        public static final int op4=0x7f0b0015;
+        public static final int radioGroup1=0x7f0b0000;
+        public static final int rbEasy=0x7f0b0008;
+        public static final int rbHard=0x7f0b000a;
+        public static final int rbLearnTech=0x7f0b0001;
+        public static final int rbMedium=0x7f0b0009;
+        public static final int rbSkillTesting=0x7f0b0002;
+        public static final int s=0x7f0b0018;
+        public static final int textView1=0x7f0b001d;
+        public static final int textView2=0x7f0b0006;
+        public static final int tvQues1=0x7f0b0011;
     }
     public static final class layout {
-        public static final int aboutus=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int learnaddition=0x7f030002;
-        public static final int learndiv=0x7f030003;
-        public static final int learnmul=0x7f030004;
-        public static final int learnsub=0x7f030005;
-        public static final int lessgreaterthan=0x7f030006;
-        public static final int level=0x7f030007;
-        public static final int main=0x7f030008;
-        public static final int questions=0x7f030009;
-        public static final int score=0x7f03000a;
-        public static final int signup=0x7f03000b;
-        public static final int splash=0x7f03000c;
-        public static final int splashtest=0x7f03000d;
+        public static final int aboutus=0x7f040000;
+        public static final int activity_main=0x7f040001;
+        public static final int learnaddition=0x7f040002;
+        public static final int learndiv=0x7f040003;
+        public static final int learnmul=0x7f040004;
+        public static final int learnsub=0x7f040005;
+        public static final int lessgreaterthan=0x7f040006;
+        public static final int level=0x7f040007;
+        public static final int main=0x7f040008;
+        public static final int questions=0x7f040009;
+        public static final int score=0x7f04000a;
+        public static final int signup=0x7f04000b;
+        public static final int splash=0x7f04000c;
+        public static final int splashtest=0x7f04000d;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int activity_main=0x7f0a0000;
+        public static final int main=0x7f0a0001;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class raw {
-        public static final int abriowin=0x7f040000;
+        public static final int abriowin=0x7f050000;
     }
     public static final class string {
-        public static final int Copyright=0x7f060000;
-        public static final int Levels=0x7f060001;
-        public static final int MathAssessment=0x7f060002;
-        public static final int aboutUs=0x7f060003;
-        public static final int addskill=0x7f060004;
-        public static final int app_name=0x7f060005;
-        public static final int divskill=0x7f060006;
-        public static final int hello_world=0x7f060007;
-        public static final int menu_settings=0x7f060008;
-        public static final int mulskill=0x7f060009;
-        public static final int questions=0x7f06000a;
-        public static final int reportCard=0x7f06000b;
-        public static final int subskill=0x7f06000c;
+        public static final int Copyright=0x7f080000;
+        public static final int Levels=0x7f080001;
+        public static final int MathAssessment=0x7f080002;
+        public static final int aboutUs=0x7f080003;
+        public static final int addskill=0x7f080004;
+        public static final int app_name=0x7f080005;
+        public static final int divskill=0x7f080006;
+        public static final int hello_world=0x7f080007;
+        public static final int menu_settings=0x7f080008;
+        public static final int mulskill=0x7f080009;
+        public static final int questions=0x7f08000a;
+        public static final int reportCard=0x7f08000b;
+        public static final int subskill=0x7f08000c;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f090000;
     }
 }
