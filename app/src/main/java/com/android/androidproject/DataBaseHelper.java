f3270d9aa030be4e5fa10291062f59face14857e
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-09 14:09:34 -0700

    DatabaseHelper

--- app/src/main/java/com/android/androidproject/DataBaseHelper.java
+++ app/src/main/java/com/android/androidproject/DataBaseHelper.java
@@ -20,7 +20,23 @@ public class DataBaseHelper extends SQLiteOpenHelper
 			_db.execSQL(com.android.androidproject.LoginDataBaseAdapter.DATABASE_CREATE);
 			
 	}
+	// Called when there is a database version mismatch meaning that the version
+	// of the database on disk needs to be upgraded to the current version.
+	@Override
+	public void onUpgrade(SQLiteDatabase _db, int _oldVersion, int _newVersion) 
+	{
+			// Log the version upgrade.
+			Log.w("TaskDBAdapter", "Upgrading from version " +_oldVersion + " to " +_newVersion + ", which will destroy all old data");
 	
+	
+			// Upgrade the existing database to conform to the new version. Multiple
+			// previous versions can be handled by comparing _oldVersion and _newVersion
+			// values.
+			// The simplest case is to drop the old table and create a new one.
+			_db.execSQL("DROP TABLE IF EXISTS " + "TEMPLATE");
+			// Create a new one.
+			onCreate(_db);
+	}
 	
 
 }
