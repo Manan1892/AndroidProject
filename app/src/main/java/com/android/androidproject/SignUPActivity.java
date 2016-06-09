6cfcbc24e7b046ce6fa89b670141e5465d5e332b
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-09 02:09:28 -0700

    SignupActivity

---
+++ app/src/main/java/com/android/androidproject/SignUPActivity.java
@@ -0,0 +1,65 @@
+package com.android.androidproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignUPActivity extends Activity
+{
+	EditText editTextUserName,editTextPassword,editTextConfirmPassword;
+	Button btnCreateAccount;
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+	
+	LoginDataBaseAdapter loginDataBaseAdapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// ---Hiding titlebar---
+				requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signup);
+		
+		// get Instance  of Database Adapter
+		loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+		loginDataBaseAdapter=loginDataBaseAdapter.open();
+		
+		// Get Refferences of Views
+		editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+		editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+		editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
+		
+		btnCreateAccount=(Button)findViewById(R.id.buttonCreateAccount);
+		btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			String userName=editTextUserName.getText().toString();
+			String password=editTextPassword.getText().toString();
+			String confirmPassword=editTextConfirmPassword.getText().toString();
+			
+			// check if any of the fields are vaccant
+			if(userName.equals("")||password.equals("")||confirmPassword.equals(""))
+			{
+					Toast.makeText(getApplicationContext(), "Field Vaccant", Toast.LENGTH_LONG).show();
+					return;
+			}
+			
+		}
+	});
+}
+	
+}
