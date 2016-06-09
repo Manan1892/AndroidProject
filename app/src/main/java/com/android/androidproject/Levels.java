a379015d8d85a9b84944c7455fcb1625ef8e8dee
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-09 02:03:03 -0700

    Levels by manan

---
+++ app/src/main/java/com/android/androidproject/Levels.java
@@ -0,0 +1,43 @@
+package com.android.androidproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class Levels extends Activity{
+
+	RadioButton rbEasy,rbMedium,rbHard;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.level);
+		initialize();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+		rbEasy = (RadioButton) findViewById(R.id.rbEasy);
+		rbMedium = (RadioButton) findViewById(R.id.rbMedium);
+		rbHard = (RadioButton) findViewById(R.id.rbHard);
+		
+	}
+	/*
+	 * @param v
+	 * private btnStarttest Click
+	 */
+	public void btnStratTest(View v)
+	
+
+}
