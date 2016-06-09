ee8b76b9655c083b90b2528eca5d913705fc62b0
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-09 02:10:56 -0700

    Splash by manan

---
+++ app/src/main/java/com/android/androidproject/Splash.java
@@ -0,0 +1,49 @@
+package com.android.androidproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splash extends Activity {
+	MediaPlayer ourSong;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		// ---Hiding titlebar---
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.splash);
+		ourSong = MediaPlayer.create(Splash.this, R.raw.abriowin);
+		ourSong.start();
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openHitCount = new Intent(Splash.this, HomeActivity.class);
+					startActivity(openHitCount);
+				}
+			}
+		};
+		timer.start();
+	}
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
