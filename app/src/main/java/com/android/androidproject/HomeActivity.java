14934f52d2ea1264f21d6d3f16a1bea3aac87ae2
Rajnish Kumar <onlinerajnish@yahoo.com>
2016-06-09 14:12:58 -0700

    HomeActivity by manan

--- app/src/main/java/com/android/androidproject/HomeActivity.java
+++ app/src/main/java/com/android/androidproject/HomeActivity.java
@@ -20,5 +20,61 @@ public class HomeActivity extends Activity {
 		finish();
 	}
 
-	
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		// ---Hiding titlebar---
+				requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// create a instance of SQLite Database
+		loginDataBaseAdapter = new com.android.androidproject.LoginDataBaseAdapter(this);
+		loginDataBaseAdapter = loginDataBaseAdapter.open();
+
+		// Get The Refference Of Buttons
+		btnSignIn = (Button) findViewById(R.id.buttonSignIN);
+		btnSignUp = (Button) findViewById(R.id.buttonSignUP);
+		final EditText editTextUserName = (EditText) findViewById(R.id.editTextUserNameToLogin);
+		final EditText editTextPassword = (EditText) findViewById(R.id.editTextPasswordToLogin);
+
+		// Set OnClick Listener on SignUp button
+		btnSignUp.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				// / Create Intent for SignUpActivity and Start The Activity
+				Intent intentSignUP = new Intent(getApplicationContext(),
+						com.android.androidproject.SignUPActivity.class);
+				startActivity(intentSignUP);
+			}
+		});
+		btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// get The User name and Password
+				String userName = editTextUserName.getText().toString();
+				String password = editTextPassword.getText().toString();
+
+				// fetch the Password form database for respective user name
+				String storedPassword = loginDataBaseAdapter
+						.getSinlgeEntry(userName);
+
+				// check if the Stored password matches with Password entered by
+				// user
+				if (password.equals(storedPassword)) {
+					Toast.makeText(HomeActivity.this,
+							"Congrats: Login Successfull", Toast.LENGTH_LONG)
+							.show();
+					Intent startMainActivity = new Intent(HomeActivity.this, MainActivity.class);
+					startActivity(startMainActivity);
+					// dialog.dismiss();
+				} else {
+					Toast.makeText(HomeActivity.this,
+							"User Name or Password does not match",
+							Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
